ae828036c411cdb2cb3831f772d95cba37a2eb4d
Javran Cheng <Javran.c@gmail.com>
2014-02-27 03:50:21 -0500

    deploy script

--- do-deploy.sh
+++ do-deploy.sh
@@ -1,10 +1,28 @@
 #!/bin/bash
 
 SOURCE='site-generated/'
-#TARGET='javran.github.io/'
-TARGET='xxx/'
+TARGET='javran.github.io/'
 
-echo '>>> Updating submodule ...'
+TIMESTAMP=`date`
 
-rsync -cvvr --delete  --exclude=.git/  "${SOURCE}" "${TARGET}"
+echo '>>>> Updating submodule ...'
 
+rsync -cvr --delete  --exclude=.git/ "${SOURCE}" "${TARGET}" \
+    || { echo 'rsync failed' ; exit 1; }
+
+echo '>>>> Committing changes ...'
+
+cd "${TARGET}"
+
+git add . --all \
+    || { echo 'git add failed' ; exit 1; }
+
+git commit -m "Updated: ${TIMESTAMP}" \
+    || { echo 'git commit failed' ; exit 1; }
+
+echo '>>>> Pushing changes ...'
+
+git push origin master \
+    || { echo 'git push failed'; exit 1; } 
+
+echo '>>>> Done'
